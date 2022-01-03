91152036e63b79449cf160847d791d54c7b49d88
CaioHTeixeira <caioteixeiraaa@gmail.com>
2022-01-03 02:06:24 -0300

    telas login e cadastro usuario

---
+++ script/login.js
@@ -0,0 +1,12 @@
+const btnLogin = document.querySelector("#signin");
+//const btnLogar = document.querySelector("#btnLogar");
+const btnCadastrar = document.querySelector("#signup");
+const body = document.querySelector("body");
+
+btnLogin.addEventListener("click", () => {
+   body.className = "sign-in-js";
+});
+
+btnCadastrar.addEventListener("click", () =>  {
+    body.className = "sign-up-js";
+})
