a50e98421a2bf89b81cf0fbb96f272781d61246a
CaioHTeixeira <caioteixeiraaa@gmail.com>
2022-02-14 03:23:14 -0300

    refactor

---
+++ server/app.js
@@ -0,0 +1,20 @@
+// variáveis globais deste módulo
+const express = require("express");
+
+const PORT = 3000
+const db = require("./db");
+const app = express()
+
+app.use(express.static('client'))
+// http://localhost:3000/login.html
+
+app.get('/', (req, res) => res.send('Hello world'));
+app.listen(PORT, () => {
+    console.log(`Estou ouvindo na porta ${PORT}`);
+  })
+
+  async function teste () {
+    const pessoas = await db.selectPessoa();
+    console.log(pessoas)
+  }
+  teste()
